d896699fe18fc2b2a1e5e179220f359a87751fcd
Ruhab Ayeshah <ruhab.ayeshah2212@gmail.com>
2026-05-14 17:37:16 +0500

    UI simplification and trailing error fixed

--- frontend/src/pages/Assessments.jsx
+++ frontend/src/pages/Assessments.jsx
@@ -77,7 +77,7 @@ export default function Assessments(){
             }
 
 
-            const res = await fetch('http://localhost:8000/api/assessments',{
+            const res = await fetch('http://localhost:8000/api/assessments/',{
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(data)
